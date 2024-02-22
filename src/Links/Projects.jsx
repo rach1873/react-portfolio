4f508227cdd3ea0a9830b9f953391f77e86d9d84
Brandon Smith <crescendobs@yahoo.com>
2024-02-21 23:14:24 -0600

    adding react-native project

--- src/Links/Projects.jsx
+++ src/Links/Projects.jsx
@@ -3,11 +3,60 @@ import css from "../images/skills/css.svg";
 import js from "../images/skills/javascript.svg";
 import react from "../images/skills/react.svg";
 import next from "../images/skills/nextjs.svg";
+import reactnative from "../images/skills/reactnative.png";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
-    <main className="text-white w-screen min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-600 to-emerald-600 space-y-8">
+    // <main className="p-8 text-white w-screen min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-600 to-emerald-600 space-y-8">
+    //   <Link
+    //     to="/projects/html"
+    //     className="flex hover:scale-125 duration-700 cursor-pointer space-x-2"
+    //   >
+    //     <img
+    //       src={html}
+    //       alt="html"
+    //       className="w-48 h-48 max-md:w-32 max-md:h-32"
+    //     />
+    //     <img
+    //       src={css}
+    //       alt="css"
+    //       className="w-48 h-48 max-md:w-32 max-md:h-32"
+    //     />
+    //     <img
+    //       src={js}
+    //       alt="html"
+    //       className="w-48 h-48 max-md:w-32 max-md:h-32"
+    //     />
+    //     <h1 className="self-center">HTML CSS and JavaScript</h1>
+    //   </Link>
+    //   <Link
+    //     to="/projects/react"
+    //     className="flex hover:scale-125 duration-700 cursor-pointer space-x-2"
+    //   >
+    //     <img src={react} alt="" className="w-48 h-48 max-md:w-32 max-md:h-32" />
+    //     <h1 className="self-center">React</h1>
+    //   </Link>
+    //   <Link
+    //     to="/projects/nextjs"
+    //     className="flex hover:scale-125 duration-700 cursor-pointer space-x-2"
+    //   >
+    //     <img src={next} alt="" className="w-48 h-48 max-md:w-32 max-md:h-32" />
+    //     <h1 className="self-center">Nextjs</h1>
+    //   </Link>
+    //   <Link
+    //     to="/projects/reactnative"
+    //     className="hover:scale-125 duration-700 cursor-pointer space-x-2"
+    //   >
+    //     <img
+    //       src={reactnative}
+    //       alt=""
+    //       className="w-48 h-48 max-md:w-32 max-md:h-32"
+    //     />
+    //     <h1 className="self-center">Nextjs</h1>
+    //   </Link>
+    // </main>
+    <main className="grid grid-cols-2 p-8 place-items-center text-white min-h-screen bg-gradient-to-r from-blue-600 to-emerald-600 space-y-8 max-lg:grid-cols-1">
       <Link
         to="/projects/html"
         className="flex hover:scale-125 duration-700 cursor-pointer space-x-2"
@@ -42,6 +91,20 @@ const Projects = () => {
       >
         <img src={next} alt="" className="w-48 h-48 max-md:w-32 max-md:h-32" />
         <h1 className="self-center">Nextjs</h1>
+      </Link>
+      <Link
+        to="/projects/reactnative"
+        className="flex hover:scale-125 duration-700 cursor-pointer space-x-2"
+      >
+        <img
+          src={reactnative}
+          alt=""
+          className="w-48 h-48 max-md:w-32 max-md:h-32"
+        />
+        <div className="flex flex-col justify-center">
+          <p>React</p>
+          <p>Native</p>
+        </div>
       </Link>
     </main>
   );
---
+++ src/Links/ReactNative.jsx
@@ -0,0 +1,25 @@
+import Project from "../components/Project";
+import weatherapp from "../images/projectimg/weatherapp.png";
+import { useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import qr from "../images/qr.png";
+
+const Rproject = () => {
+  const navigate = useNavigate();
+  return (
+    <main className="min-h-screen flex flex-col items-center text-white p-8 bg-gradient-to-r from-blue-600 to-emerald-600">
+      <p className="text-lg mb-9">
+        Scan the QR below to view Project.
+        <span className="text-red-400">
+          This app can only be viewed on an IOS device
+        </span>
+      </p>
+      <div>
+        <img src={qr} alt="QR Code" className="w-52 h-52" />
+      </div>
+      <BackButton />
+    </main>
+  );
+};
+
+export default Rproject;
