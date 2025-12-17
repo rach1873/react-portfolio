223cab24f957db3a1a93556e745d1acb184ba4a8
Brandon Smith <crescendobs@yahoo.com>
2025-12-16 23:50:38 -0600

    add pdf

--- src/Links/Resume.jsx
+++ src/Links/Resume.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import resume from "../pdf/btsmithresume.pdf";
+import resume from "../pdf/btsmithresumepdf.pdf";
 
 const Resume = () => {
   const h = window.innerHeight;
